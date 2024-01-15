0987527c79b30ef6e2bdc0fed61d381eb68b4517
A-M-U-R <daniil.baimurzin@mail.ru>
2024-01-15 16:48:29 +0500

    Header

---
+++ src/components/UI/DropBox/index.tsx
@@ -0,0 +1,37 @@
+import {Menu, Transition} from '@headlessui/react'
+import React from "react";
+import styles from "./style.module.css"
+
+
+export function DropBox() {
+    return (
+        <Menu>
+            <Menu.Button>More</Menu.Button>
+            <Menu.Items>
+                <Menu.Item>
+                    {({ active }) => (
+                        <a
+                            className={`${active && 'bg-blue-500'}`}
+                            href="/account-settings"
+                        >
+                            Account settings
+                        </a>
+                    )}
+                </Menu.Item>
+                <Menu.Item>
+                    {({ active }) => (
+                        <a
+                            className={`${active && 'bg-blue-500'}`}
+                            href="/account-settings"
+                        >
+                            Documentation
+                        </a>
+                    )}
+                </Menu.Item>
+                <Menu.Item disabled>
+                    <span className="opacity-75">Invite a friend (coming soon!)</span>
+                </Menu.Item>
+            </Menu.Items>
+        </Menu>
+    )
+}
